96d22a051e720f6a17a4354787f8613275c0c5d8
Kevin Campos Venegas <kenderman.8@gmail.com>
2023-07-20 17:49:40 -0400

    working sail

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,5 +24,8 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call(UsersSeeder::class);
+        $this->call(TipoEventoSeeder::class);
+        $this->call(TipoModuloSeeder::class);
+
     }
 }
